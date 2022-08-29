a33755c43933b5baab208cff0af011a515d16334
Jhonatan Sant'Ana <jhonatansantana.foto@gmail.com>
2022-08-29 17:08:44 -0300

    Atualiza responsividade; Remove outline; Remove botão colar

---
+++ jsscript.js
@@ -0,0 +1,30 @@
+const text = document.querySelector(".text");
+const modifiedText = document.querySelector(".modifiedText");
+const encoder = [["e","enter"] , ["i","imes"] , ["a","ai"] , ["o","ober"] , ["u","ufat"]]
+
+function encrypt(){
+    let encryptText = text.value;
+    for (i = 0; i < encoder.length; i++){
+        if(text.value.includes(encoder[i][0])){    
+            encryptText = encryptText.replaceAll(encoder[i][0] , encoder[i][1]);
+            modifiedText.value = encryptText;
+        }      
+    }
+}
+
+function decrypt() {
+    let decryptText = text.value;
+    for (i = 0; i < encoder.length; i++){
+        if(text.value.includes(encoder[i][1])){
+            decryptText = decryptText.replaceAll(encoder[i][1] , encoder[i][0]);
+            modifiedText.value = decryptText;
+        }
+    }
+}
+
+function copy(){
+    document.querySelector(".modifiedText").select();
+    document.execCommand("copy");
+    document.querySelector(".modifiedText").value="Copiado!";
+    document.querySelector(".text").select();
+}
